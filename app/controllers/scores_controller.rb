f2248853f4f196a84a3e82ec37871ce651e038a6
Carly Pecora <carly@carlypecora.com>
2019-07-08 14:19:11 -0400

    set up backend

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,24 @@
+class ScoresController < ApplicationController
+
+	def index
+		@scores = Score.all 
+		render json: @scores
+	end
+
+	def create
+		@score = Score.create(score_params)
+		render json: @score 
+	end
+
+	def show
+		@score = Score.find(params[:id])
+		render json: @score
+	end
+
+	private
+
+	def score_params
+		params.require(:score).permit(:score)
+	end
+
+end
